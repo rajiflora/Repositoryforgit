217df96588c785e3841b277b9c2ccc234900a29f
raji <pappu150210@gmail.com>
2022-03-28 16:06:00 -0400

    added test.html

---
+++ helloworld_raji/src/test/java/helloworld_raji/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld_raji;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void test() {
+		System.out.println(".... Hello World!...");
+	}
+
+}
